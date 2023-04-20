501296888f125a1b141705a13832a40c6ad78547
luka mkalavishvili <mkalavishvililuka00@gmail.com>
2023-04-20 15:47:00 +0400

    merge v2

--- src/components/Forms/FaqEdit.jsx
+++ src/components/Forms/FaqEdit.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { React, useRef, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import FaqInput from '../../components/Forms/inputs/FaqInput';
 
-function FaqEdit() {
-  return <h1 className="text-[red]">hello</h1>;
+function FaqEdit(props) {
+  
+  const category = useRef()
+  const heading = useRef()
+  const [content, setContent] = useState('');
+
+  const options = [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline'],
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'color': [] },],
+    [{ 'align': [] }],
+    ['clean']
+  ];
+
+  const modules = {
+    toolbar: options
+  }
+
+  function addFAQ(){
+    console.log(category.current.value);
+    console.log(heading.current.value);
+    console.log(content);
+  }
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8 flex flex-col gap-[30px]">
+
+      <FaqInput
+          name="category"
+          label="Category"
+          inpRef={category}
+      />
+
+      <FaqInput
+          name="heading"
+          label="Heading"
+          inpRef={heading}
+      />
+
+      <ReactQuill modules={modules} theme="snow" value={content} onChange={setContent} className='text-black'/>
+
+      <button
+          onClick={()=> addFAQ()}
+          className="max-w-fit inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+      </button>
+    </div>
+  )
 }
 
-export default FaqEdit;
+export default FaqEdit
